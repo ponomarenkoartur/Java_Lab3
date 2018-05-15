4104650c34cc753d2133e44cb406bde96615a7c0
Artur Ponomarenko <ponomarenkoartur@protonmail.com>
2018-05-15 12:57:44 +0300

    Create convert1 and convert2 methods in Part5 class

--- src/ua/nure/ponomarenko/task3/Part5.java
+++ src/ua/nure/ponomarenko/task3/Part5.java
@@ -4,30 +4,70 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Part5 {
+    private static Pattern loginPattern = Pattern.compile("(^|(?<=\\n))[a-zA-Zа-яА-Я]+(?=;)");
+    private static Pattern namePattern = Pattern.compile("(?<=;)[a-zA-Z]+\\s[a-zA-Z]+(?=;)");
+    private static Pattern emailPattern = Pattern.compile("(?<=;)\\S+$");
+    private static Pattern domainPattern = Pattern.compile("(?<=@)[a-zA-Z]+\\.[a-zA-Z]+$");
+    private static Pattern nameAndEmailPattern = Pattern.compile("(?<=;)[a-zA-Zа-яА-Я]+\\s[a-zA-Zа-яА-Я]+(?=;)|(?<=;)\\S+($|(?=\\n))");
+    private static Pattern loginAndEmailPattern = Pattern.compile("(^|(?<=\\n))[a-zA-Zа-яА-Я]+(?=;)|(?<=;)\\S+($|(?=\\n))");
+
+
     public static void main(String[] args) {
-        String input = "ivanov;Ivan Ivanov;ivanov@mail.ru";
-        System.out.println(convert1(input));
+        String input = "ivanov;Ivan Ivanov;ivanov@mail.ru\n" +
+                "петров;Петр Петров;petrov@google.com\n" +
+                "lennon;John Lennon;lennon@google.com";
+        System.out.println(convert2(input));
     }
 
     public static String convert1(String input) {
         StringBuilder stringBuilder = new StringBuilder();
 
-        Pattern loginPattern = Pattern.compile("^\\w+(?=;)");
-        Pattern emailPattern = Pattern.compile("(?<=;)\\S+$");
+        Matcher matcher = loginAndEmailPattern .matcher(input);
+        for (int i = 0; matcher.find(); i++) {
+            stringBuilder.append(matcher.group());
+            if (i % 2 == 0) {
+                stringBuilder.append(" ==> ");
+            } else {
+                stringBuilder.append("\n");
+            }
+        }
 
-        Matcher matcher = loginPattern.matcher(input);
-        // Find login
+        return stringBuilder.toString();
+    }
+
+    public static String convert2(String input) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        Matcher matcher = nameAndEmailPattern.matcher(input);
+        for (int i = 0; matcher.find(); i++) {
+            stringBuilder.append(matcher.group());
+            if (i % 2 == 0) {
+                stringBuilder.append(" (email: ");
+            } else {
+                stringBuilder.append(")\n");
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public static String convert3(String input) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        // Find domain
+        Matcher matcher = domainPattern.matcher(input);
         if (matcher.find()) {
-            // Append login
+            // Append domain
             stringBuilder.append(matcher.group());
             stringBuilder.append(" ==> ");
         }
 
-        // Find email
-        matcher = emailPattern.matcher(input);
+        // Find login
+        matcher = loginPattern.matcher(input);
         if (matcher.find()) {
-            // Append email
+            // Append login
             stringBuilder.append(matcher.group());
+            stringBuilder.append(")");
         }
 
         return stringBuilder.toString();
