d1bc5febc80fe7384b474a4804a7d05baebb1b92
Artur Ponomarenko <ponomarenkoartur@protonmail.com>
2018-05-15 14:51:07 +0300

    Refactor Part5 class. Create FileReader class.

--- src/ua/nure/ponomarenko/task3/Part5.java
+++ src/ua/nure/ponomarenko/task3/Part5.java
@@ -4,27 +4,20 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Part5 {
-    private static Pattern loginPattern = Pattern.compile("(^|(?<=\\n))[a-zA-Zа-яА-Я]+(?=;)");
-    private static Pattern namePattern = Pattern.compile("(?<=;)[a-zA-Z]+\\s[a-zA-Z]+(?=;)");
-    private static Pattern emailPattern = Pattern.compile("(?<=;)\\S+$");
-    private static Pattern domainPattern = Pattern.compile("(?<=@)\\w+\\.\\w+($|(?=\\n))");
-
-    private static Pattern nameAndEmailPattern = Pattern.compile("(?<=;)[a-zA-Zа-яА-Я]+\\s[a-zA-Zа-яА-Я]+(?=;)|(?<=;)\\S+($|(?=\\n))");
-    private static Pattern loginAndEmailPattern = Pattern.compile("(^|(?<=\\n))[a-zA-Zа-яА-Я]+(?=;)|(?<=;)\\S+($|(?=\\n))");
-
-
     public static void main(String[] args) {
         String input = "ivanov;Ivan Ivanov;ivanov@mail.ru\n" +
                 "петров;Петр Петров;petrov@google.com\n" +
                 "lennon;John Lennon;lennon@google.com";
-//        System.out.println(convert2(input));
-        System.out.println(convert3(input));
+        System.out.println(convert2(input));
     }
 
     public static String convert1(String input) {
         StringBuilder stringBuilder = new StringBuilder();
 
-        Matcher matcher = loginAndEmailPattern.matcher(input);
+        Pattern loginOrEmailPattern = Pattern.compile("(^|(?<=\\n))[a-zA-Zа-яА-Я]+(?=;)|(?<=;)\\S+($|(?=\\n))");
+        Matcher matcher = loginOrEmailPattern.matcher(input);
+
+        // Find login or email by turn
         for (int i = 0; matcher.find(); i++) {
             stringBuilder.append(matcher.group());
             if (i % 2 == 0) {
@@ -40,7 +33,10 @@ public class Part5 {
     public static String convert2(String input) {
         StringBuilder stringBuilder = new StringBuilder();
 
-        Matcher matcher = nameAndEmailPattern.matcher(input);
+        Pattern nameOrEmailPattern = Pattern.compile("(?<=;)[a-zA-Zа-яА-Я]+\\s[a-zA-Zа-яА-Я]+(?=;)|(?<=;)\\S+($|(?=\\n))");
+        Matcher matcher = nameOrEmailPattern.matcher(input);
+
+        // Find name or email by turn
         for (int i = 0; matcher.find(); i++) {
             stringBuilder.append(matcher.group());
             if (i % 2 == 0) {
@@ -58,28 +54,37 @@ public class Part5 {
 
         String usedDomains = "";
 
+        Pattern domainPattern = Pattern.compile("(?<=@)\\w+\\.\\w+($|(?=\\n))");
         Matcher domainMatcher = domainPattern.matcher(input);
 
+        // Find domains
         while (domainMatcher.find()) {
             String domain = domainMatcher.group();
 
-            if (!usedDomains.contains(domain)) {
-                usedDomains += domain + ";";
-
-                Pattern loginFollowedByDomainPatter = Pattern.compile("(^|(?<=\\n))[a-zA-Zа-яА-Я]+(?=;.+;.+" + domain + ")");
-                Matcher loginMatcher = loginFollowedByDomainPatter.matcher(input);
-
-                stringBuilder.append(domain).append(" ==> ");
-
-                for (int i = 0; loginMatcher.find(); i++) {
-                    stringBuilder.append(loginMatcher.group()).append(", ");
-                }
-                stringBuilder.append("\n");
+            if (usedDomains.contains(domain)) {
+                break;
             }
+
+            usedDomains += domain + ";";
+
+            Pattern loginForDomainPattern = Pattern.compile("(^|(?<=\\n))[a-zA-Zа-яА-Я]+(?=;.+;.+" + domain + ")");
+            Matcher loginMatcher = loginForDomainPattern.matcher(input);
+            stringBuilder.append(domain).append(" ==> ");
+
+            // Find logins for specified domain
+            for (int i = 0; loginMatcher.find(); i++) {
+                stringBuilder.append(loginMatcher.group());
+                stringBuilder.append(", ");
+            }
+            stringBuilder.append("\n");
+
+            // Delete last ', ' characters in the line
+            stringBuilder.delete(stringBuilder.length() - 3, stringBuilder.length() - 1);
         }
 
+        // Delete last '\n' character in the text
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
 
         return stringBuilder.toString();
     }
-
 }
